150350b7d2e23e39c603751b6d91effe0d892b14
Aknazew686 <Aknazew686@gmail.com>
2020-03-08 01:53:56 +0300

    добавляет модули

---
+++ js/map.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function () {
+  var templateCard = document.querySelector('#card').content.querySelector('.map__card');
+  var isActiveMap = false;
+  var map = document.querySelector('.map');
+
+  var getActiveMap = function () {
+    if (isActiveMap) {
+      return;
+    }
+    map.classList.remove('map--faded');
+    map.appendChild(window.pin.renderPins());
+    window.form.toggleDisabled(false);
+    window.form.adForm.querySelector('form').classList.remove('ad-form--disabled');
+    isActiveMap = true;
+  };
+
+  map.appendChild(templateCard);
+
+  window.form.mapPinClickHandler.addEventListener('mousedown', function (evt) {
+    if (evt.which === window.const.CLICK_MOUSE_LEFT) {
+      getActiveMap();
+    };
+  });
+
+  window.form.mapPinClickHandler.addEventListener('keydown', function (evt) {
+    if (evt.key === window.const.ENTER_KEY) {
+      getActiveMap();
+    }
+  });
+
+  window.map = {
+    map: map,
+    templateCard
+  };
+})();
