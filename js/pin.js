ade18d3537a75e475ee7dc4bb5be4792b1da28b1
Aknazew686 <Aknazew686@gmail.com>
2020-03-09 20:08:25 +0300

    вносит изменения

--- js/pin.js
+++ js/pin.js
@@ -1,68 +1,9 @@
 'use strict';
 
 (function () {
-  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
   var AVATAR_WIDTH = 40;
   var AVATAR_HEIGHT = 40;
   var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
-
-  var randomNumber = function (min,max) {
-    var rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  };
-
-  var randomArr = function (arr) {
-    var randomArr = [];
-    var copyArr = arr.slice();
-    var count =  randomNumber(1,copyArr.length - 1)
-
-    for (var i = 0; i < count; i++) {
-      var randomIndex = randomNumber(0,copyArr.length - 1);
-
-      randomArr.push(copyArr[randomIndex]);
-      copyArr.splice(randomIndex, 1);
-    }
-
-    return randomArr;
-  };
-
-  var createPin = function (i) {
-    return {
-      author: {
-        avatar: 'img/avatars/user0' +  (i + 1) + '.png'
-      },
-      offer: {
-        title: 'объявление',
-        address: '600, 350',
-        price: randomNumber(0,4000),
-        type: 'house',
-        rooms: randomNumber(1,4),
-        guests: randomNumber(1,8),
-        checkin: '12:00',
-        checkout: '14:00',
-        features: randomArr(FEATURES),
-        description: 'Сдается дом для проживания',
-        photos: randomArr(PHOTOS)
-      },
-      location: {
-        x: randomNumber(1,1200),
-        y: randomNumber(130,630)
-      }
-    }
-  };
-
-  var createPins = function () {
-    var pins = [];
-
-    for (var i = 0; i < 8; i++) {
-      pins.push(createPin(i))
-    }
-
-    return pins;
-  };
-
-  var pins = createPins();
 
   var renderPin = function(index,fragment) {
     var pinElement = templatePin.cloneNode(true);
@@ -86,7 +27,7 @@
       })
   }
 
-  var renderPins = function () {
+  var renderPins = function (pins) {
     var fragment = document.createDocumentFragment();
 
     for (var i = 0; i < pins.length; i++) {
